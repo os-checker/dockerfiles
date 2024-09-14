11de2507d76312f0d8d4aab2f32da05e31e7df70
zjp <jiping_zhou@foxmail.com>
2024-09-14 08:43:02 +0800

    mirai: Byte-OS/polyhal build on riscv64gc-unknown-none-elf by default

--- mirai.sh
+++ mirai.sh
@@ -1,6 +1,8 @@
 git clone https://github.com/Byte-OS/polyhal.git
 cd polyhal
 
+rustup +nightly-2023-12-30 target add riscv64gc-unknown-none-elf
+
 # time MIRAI_FLAGS="--diag=paranoid" cargo +nightly-2023-12-30 mirai
 #
 # cargo clean
@@ -19,6 +21,6 @@ time MIRAI_FLAGS="--diag=default" cargo +nightly-2023-12-30 mirai
 # echo "--diag=default"
 # time MIRAI_FLAGS="--statistics --diag=default" cargo +nightly-2023-12-30 mirai
 
-rustup +nightly-2023-12-30 target add riscv64gc-unknown-none-elf
+rustup +nightly-2023-12-30 target add x86_64-unknown-none
 cargo clean
-time MIRAI_FLAGS="--diag=default" cargo +nightly-2023-12-30 mirai --target riscv64gc-unknown-none-elf
+time MIRAI_FLAGS="--diag=default" cargo +nightly-2023-12-30 mirai --target x86_64-unknown-none
