5a3a8f46dfe02286e40c066ecfa814e828d94f18
zjp <jiping_zhou@foxmail.com>
2024-09-14 09:28:55 +0800

    chore: _github_mirai_layer/ => .github/; df -alh

--- mirai.sh
+++ mirai.sh
@@ -1,6 +1,8 @@
 git clone https://github.com/Byte-OS/polyhal.git
 cd polyhal
 
+df -alh
+
 rustup +nightly-2023-12-30 target add riscv64gc-unknown-none-elf
 
 # time MIRAI_FLAGS="--diag=paranoid" cargo +nightly-2023-12-30 mirai
@@ -21,6 +23,10 @@ time MIRAI_FLAGS="--diag=default" cargo +nightly-2023-12-30 mirai
 # echo "--diag=default"
 # time MIRAI_FLAGS="--statistics --diag=default" cargo +nightly-2023-12-30 mirai
 
+df -alh
+
 rustup +nightly-2023-12-30 target add x86_64-unknown-none
 cargo clean
 time MIRAI_FLAGS="--diag=default" cargo +nightly-2023-12-30 mirai --target x86_64-unknown-none
+
+df -alh
