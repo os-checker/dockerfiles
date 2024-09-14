fcd52b77964c1286248920d9b7e60775bdb834bc
zjp <jiping_zhou@foxmail.com>
2024-09-14 08:28:36 +0800

    mirai: test --target

--- mirai.sh
+++ mirai.sh
@@ -1,20 +1,24 @@
-git clone https://github.com/kern-crates/rtc.git
-cd rtc
+git clone https://github.com/Byte-OS/polyhal.git
+cd polyhal
 
-time MIRAI_FLAGS="--diag=paranoid" cargo +nightly-2023-12-30 mirai
-
-cargo clean
+# time MIRAI_FLAGS="--diag=paranoid" cargo +nightly-2023-12-30 mirai
+#
+# cargo clean
 time MIRAI_FLAGS="--diag=default" cargo +nightly-2023-12-30 mirai
 
+# cargo clean
+# echo "--diag=paranoid"
+# time MIRAI_FLAGS="--statistics --diag=paranoid" cargo +nightly-2023-12-30 mirai
+# cargo clean
+# echo "--diag=library"
+# time MIRAI_FLAGS="--statistics --diag=library" cargo +nightly-2023-12-30 mirai
+# cargo clean
+# echo "--diag=verify"
+# time MIRAI_FLAGS="--statistics --diag=verify" cargo +nightly-2023-12-30 mirai
+# cargo clean
+# echo "--diag=default"
+# time MIRAI_FLAGS="--statistics --diag=default" cargo +nightly-2023-12-30 mirai
+
+rustup +nightly-2023-12-30 target add riscv64gc-unknown-none-elf
 cargo clean
-echo "--diag=paranoid"
-time MIRAI_FLAGS="--statistics --diag=paranoid" cargo +nightly-2023-12-30 mirai
-cargo clean
-echo "--diag=library"
-time MIRAI_FLAGS="--statistics --diag=library" cargo +nightly-2023-12-30 mirai
-cargo clean
-echo "--diag=verify"
-time MIRAI_FLAGS="--statistics --diag=verify" cargo +nightly-2023-12-30 mirai
-cargo clean
-echo "--diag=default"
-time MIRAI_FLAGS="--statistics --diag=default" cargo +nightly-2023-12-30 mirai
+time MIRAI_FLAGS="--diag=default" cargo +nightly-2023-12-30 mirai --target riscv64gc-unknown-none-elf
