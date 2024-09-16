7c385c7b132a253154831ee7c4b00da944dae6ad
zjp <jiping_zhou@foxmail.com>
2024-09-16 09:24:46 +0800

    mirai: free -h 查看内存使用情况

--- mirai.sh
+++ mirai.sh
@@ -1,7 +1,7 @@
 git clone https://github.com/Byte-OS/polyhal.git
 cd polyhal
 
-df -alh
+df -alh && free -h
 
 rustup +nightly-2023-12-30 target add riscv64gc-unknown-none-elf
 
@@ -23,10 +23,10 @@ time MIRAI_FLAGS="--diag=default" cargo +nightly-2023-12-30 mirai
 # echo "--diag=default"
 # time MIRAI_FLAGS="--statistics --diag=default" cargo +nightly-2023-12-30 mirai
 
-df -alh
+df -alh && free -h
 
 rustup +nightly-2023-12-30 target add x86_64-unknown-none
 cargo clean
 time MIRAI_FLAGS="--diag=default" cargo +nightly-2023-12-30 mirai --target x86_64-unknown-none
 
-df -alh
+df -alh && free -h
