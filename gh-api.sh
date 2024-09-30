6fb69cb8da5ddd9cebd2e65f1683fb49e7a70f04
zjp <jiping_zhou@foxmail.com>
2024-09-30 13:27:05 +0800

    gh: XZ_OPT=-e9 tar -cJvf

--- gh-api.sh
+++ gh-api.sh
@@ -31,11 +31,17 @@ ls -alh
 # echo "成功上传 cache.redb"
 
 # install ouch
-wget https://github.com/ouch-org/ouch/releases/download/0.5.1/ouch-x86_64-unknown-linux-gnu.tar.gz
-tar -zxvf ouch-x86_64-unknown-linux-gnu.tar.gz
-mv ouch-x86_64-unknown-linux-gnu/ouch ~/.cargo/bin
+# wget https://github.com/ouch-org/ouch/releases/download/0.5.1/ouch-x86_64-unknown-linux-gnu.tar.gz
+# tar -zxvf ouch-x86_64-unknown-linux-gnu.tar.gz
+# mv ouch-x86_64-unknown-linux-gnu/ouch ~/.cargo/bin
 # compress cache.redb
-ouch c --slow cache.redb cache.redb.tar.gz
+# ouch c --slow cache.redb cache.redb.tar.gz
+XZ_OPT=-e9 tar -cJvf cache.redb.tar.xz cache.redb
 ls -alh
 gh release upload --clobber -R os-checker/database cache.redb cache.redb.tar.gz
 echo "成功上传 cache.redb.tar.gz"
+
+# 解压缩
+mkdir tmp
+tar -xJvf cache.redb.tar.xz -C tmp
+ls -alh tmp
