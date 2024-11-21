b8aeba7f76ad7424a9723a2c2dca957cb4676327
zjp <jiping_zhou@foxmail.com>
2024-11-21 13:49:35 +0800

    docker: run checkers with their toolchains

--- run.sh
+++ run.sh
@@ -3,10 +3,10 @@ rustup toolchain install $OS_CHECKER_RUST_TOOLCHAIN --allow-downgrade --profile 
 
 rustc -vV
 
-cargo rudra --help
-cargo mirai --help
-cargo rap --help
-cargo lockbud --help
+cargo +nightly-2021-10-20 rudra --help
+cargo +nightly-2024-02-04 mirai --help
+cargo +nightly-2024-10-04 lockbud --help
+cargo +nightly-2024-10-11 rap --help
 cargo audit --version
 cargo outdated --version
 cargo geiger --version
