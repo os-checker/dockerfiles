89bae9f572be74002d972d8e59953a87d37c8741
zjp <jiping_zhou@foxmail.com>
2024-11-21 12:46:58 +0800

    docker: fix clone path for each checker

--- os-checker.sh
+++ os-checker.sh
@@ -4,30 +4,30 @@ rustup toolchain install $OS_CHECKER_RUST_TOOLCHAIN --allow-downgrade --profile 
 rustc -vV
 
 # Install Rudra
-git clone https://github.com/os-checker/Rudra.git
-cd Rudra
+git clone https://github.com/os-checker/Rudra.git /tmp/rudra
+cd /tmp/rudra
 git switch master
 cargo install --path . --locked
 cargo rudra --help
 
 # Install Mirai
-git clone https://github.com/os-checker/MIRAI.git
-cd MIRAI
+git clone https://github.com/os-checker/MIRAI.git /tmp/mirai
+cd /tmp/mirai
 git switch main
 cargo install --path checker --no-default-features -F z3,z3-sys --locked
 cargo mirai --help
 
 # Install Rap
-git clone https://github.com/os-checker/RAP.git
-cd RAP
+git clone https://github.com/os-checker/RAP.git /tmp/rap
+cd /tmp/rap
 git switch main
 cd rap
 cargo install --path .
 cargo rap --help
 
 # Install Lockbud
-git clone https://github.com/os-checker/lockbud.git
-cd lockbud
+git clone https://github.com/os-checker/lockbud.git /tmp/lockbud
+cd /tmp/lockbud
 git switch all
 cargo install --path .
 cargo lockbud --help
