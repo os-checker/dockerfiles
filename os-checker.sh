5681cc90546946c11b6209e51d5a1ff1fe2278d8
zjp <jiping_zhou@foxmail.com>
2024-11-21 12:31:27 +0800

    docker: install rudra mirai rap lockbud

--- os-checker.sh
+++ os-checker.sh
@@ -2,3 +2,32 @@
 rustup toolchain install $OS_CHECKER_RUST_TOOLCHAIN --allow-downgrade --profile minimal --component clippy,rustfmt
 
 rustc -vV
+
+# Install Rudra
+git clone https://github.com/os-checker/Rudra.git
+cd Rudra
+git switch master
+cargo install --path . --locked
+cargo rudra --help
+
+# Install Mirai
+git clone https://github.com/os-checker/MIRAI.git
+cd MIRAI
+git switch main
+cargo install --path checker --no-default-features -F z3,z3-sys --locked
+cargo mirai --help
+
+# Install Rap
+git clone https://github.com/os-checker/RAP.git
+cd RAP
+git switch main
+cd rap
+cargo install --path .
+cargo rap --help
+
+# Install Lockbud
+git clone https://github.com/os-checker/lockbud.git
+cd lockbud
+git switch all
+cargo install --path .
+cargo lockbud --help
