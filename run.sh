6f5b04e22e4c6aa1183eb8f81dadd7d870b1d473
zjp <jiping_zhou@foxmail.com>
2024-11-23 21:01:04 +0800

    run.sh: push changes to DATABASE_REPO; /check/dist is for UI deployment

--- run.sh
+++ run.sh
@@ -77,3 +77,38 @@ os-checker-plugin-cargo
 
 # Generate rustdocs
 os-checker-plugin-docs
+
+# deploy -> /check/dist/docs (NOTE: WebUI should not define docs page/route)
+mv deploy dist/docs
+
+git config --global user.name $GIT_AUTHOR
+git config --global user.email $GIT_EMAIL
+git config --global committer.name $GIT_AUTHOR
+git config --global committer.email $GIT_EMAIL
+
+echo "正在 clone" $DATABASE_REPO
+gh repo clone $DATABASE_REPO $DATABASE_REPO
+gh auth setup-git
+echo "成功 clone" $DATABASE_REPO
+
+# clean old plugin dir
+rm $DATABASE_REPO/plugin -rf
+
+# cargo -> $DATABASE_REPO/plugin/cargo/info
+mkdir -p $DATABASE_REPO/plugin/cargo
+mv cargo $DATABASE_REPO/plugin/cargo/info
+
+# github-api -> $DATABASE_REPO/plugin/github-api
+mv github-api $DATABASE_REPO/plugin
+
+# commit & push changes
+echo "update plugin folder for" $DATABASE_REPO
+export branch=$(git branch --show-current)
+
+git status
+git add .
+echo "正在提交：$(git status --porcelain)"
+git commit -m "[bot] update plugin dir from os-checker-plugin-github-api repo"
+echo "提交成功，正在推送到 database 仓库（分支：$branch）"
+git push
+echo "成功推送到 database 仓库（分支：$branch）"
